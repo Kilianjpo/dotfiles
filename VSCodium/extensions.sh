d3673acfe862305712d0cfdd412a212ea6e7ddd7
Kilianjpo <82637115+Kilianjpo@users.noreply.github.com>
2025-08-30 16:45:36 +0200

    Update

---
+++ VSCodium/extensions.sh
@@ -0,0 +1,22 @@
+#!/bin/fish
+
+vscodium --install-extension aaron-bond.better-comments
+vscodium --install-extension alexdauenhauer.catppuccin-noctis-icons
+vscodium --install-extension catppuccin.catppuccin-vsc
+vscodium --install-extension charliermarsh.ruff
+vscodium --install-extension continue.continue
+vscodium --install-extension docker.docker
+vscodium --install-extension ibm.output-colorizer
+vscodium --install-extension jeanp413.open-remote-ssh
+vscodium --install-extension matthewpi.caddyfile-support
+vscodium --install-extension ms-azuretools.vscode-containers
+vscodium --install-extension ms-azuretools.vscode-docker
+vscodium --install-extension ms-python.debugpy
+vscodium --install-extension ms-python.python
+vscodium --install-extension pkief.material-product-icons
+vscodium --install-extension qufiwefefwoyn.inline-sql-syntax
+vscodium --install-extension redhat.vscode-xml
+vscodium --install-extension redhat.vscode-yaml
+vscodium --install-extension sndst00m.markdown-github-dark-pack
+vscodium --install-extension streetsidesoftware.code-spell-checker
+vscodium --install-extension tamasfe.even-better-toml
---
+++ instal.sh
@@ -0,0 +1,55 @@
+#!/bin/fish
+
+# Rate Mirrors
+sudo cachyos-rate-mirrors
+
+# Install packages
+sudo pacman -S --needed base-devel
+sudo pacman -S sbctl appmenu-gtk-module libdbusmenu-glib cachyos-emerald-kde-theme-git qogir-icon-theme ghostty 
+paru -R octopi vim vi totem rsync pv meld cachyos-packageinstaller micro gedit cachyos-zsh-config cachyos-micro-config alacritty firefox
+
+# git
+cp -rf git/.gitconfig ~/.gitconfig
+
+# ssh
+cp -rf ssh/config ~/.ssh/config
+
+# paru
+git clone https://aur.archlinux.org/paru.git paru-aur
+cd paru-aur
+makepkg -si
+cd ..
+rm -rf paru-aur
+cp -rf paru/paru.conf /etc/paru.conf
+
+# 1Password
+paru 1password
+
+# Zen-browser
+paru zen-browser-bin
+
+# Betterbird
+paru  betterbird
+
+# Spotify
+paru spotify
+
+# RelaGit
+paru relagit
+
+# Beekeeper
+paru beekeeper
+
+# VSCodium
+paru vscodium
+VSCodium/extensions.sh
+cp -rf VSCodium/settings.json ~/.config/VSCodium/User/settings.json
+
+# UFW
+sudo ufw enable
+
+# wireless-regdom
+sudo echo 'WIRELESS_REGDOM="DE"' > /etc/conf.d/wireless-regdom
+
+# tldr
+tldr --update
